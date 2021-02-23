83d71f23b94885418672696ce87d87a23462dd6a
Aria <27708042+ariawinter@users.noreply.github.com>
2021-02-23 07:01:17 -0800

    update and change to latest

--- ValheimPlus/Experience.cs
+++ ValheimPlus/Experience.cs
@@ -23,44 +23,105 @@ namespace ValheimPlus
 {
     class Experience
     {
-        
-        [HarmonyPatch(typeof(Skills), "RaiseSkill")]
-        public static class AddExpGainedDisplay
-        {
 
-            /*
-            private static void Prefix(Skills __instance, Skills.SkillType skillType, float factor = 1f)
-            {
-               
+		[HarmonyPatch(typeof(Skills), "RaiseSkill")]
+		public static class AddExpGainedDisplay
+		{
+			private static void Prefix(ref Skills __instance, ref Skills.SkillType skillType, ref float factor)
+			{
+				if (Configuration.Current.Experience.IsEnabled)
+				{
+					switch ((SkillType)skillType)
+					{
+						case SkillType.Swords:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.swords);
+							break;
+						case SkillType.Knives:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.knives);
+							break;
+						case SkillType.Clubs:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.clubs);
+							break;
+						case SkillType.Polearms:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.polearms);
+							break;
+						case SkillType.Spears:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.spears);
+							break;
+						case SkillType.Blocking:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.blocking);
+							break;
+						case SkillType.Axes:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.axes);
+							break;
+						case SkillType.Bows:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.bows);
+							break;
+						case SkillType.FireMagic:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.fireMagic);
+							break;
 
-            }*/
+						case SkillType.FrostMagic:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.frostMagic);
+							break;
 
-            private static void Postfix(Skills __instance, Skills.SkillType skillType, float factor = 1f)
-            {
-                Skills.Skill skill = __instance.GetSkill(skillType);
-                String skill_name = skill.m_info.m_skill.ToString();
+						case SkillType.Unarmed:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.unarmed);
+							break;
 
-                if (skill_name.ToString() == "900" || skill_name.ToString() == "skill_900")
-                {
-                    skill_name = "Athletics";
-                }
+						case SkillType.Pickaxes:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.pickaxes);
+							break;
 
-                var display_message = skill_name != "Athletics" && skill_name != "Run" && skill_name != "Swim" && skill_name != "Jump" && skill_name != "Sneak";
+						case SkillType.WoodCutting:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.woodCutting);
+							break;
 
-                if (display_message)
-                {
-                    if (Configuration.Current.Player.IsEnabled && Configuration.Current.Player.ExperienceGainedNotifications)
-                    {
-                        float percent = skill.m_accumulator / (skill.GetNextLevelRequirement() / 100);
-                        int level = (int)skill.m_level;
-                        //__instance.m_player.Message(MessageHud.MessageType.TopLeft, skill.m_info.m_skill + " [" + Helper.tFloat(skill.m_accumulator, 2) + "/" + Helper.tFloat(skill.GetNextLevelRequirement(), 2) + "] (" + Helper.tFloat(percent, 0) + "%)", 0, skill.m_info.m_icon);
+						case SkillType.Jump:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.jump);
+							break;
 
+						case SkillType.Sneak:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.sneak);
+							break;
 
+						case SkillType.Run:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.run);
+							break;
 
-                        __instance.m_player.Message(MessageHud.MessageType.TopLeft, skill_name + " " + level.ToString() + " (" + Helper.tFloat(percent, 0) + "%)", 0, skill.m_info.m_icon);
-                    }
-                }
-            }
-        }
-    }
+						case SkillType.Swim:
+							factor = factor + ((factor / 100) * Configuration.Current.Experience.swim);
+							break;
+
+						default:
+							break;
+					}
+				}
+			}
+
+		}
+
+		public enum SkillType
+		{
+			None,
+			Swords,
+			Knives,
+			Clubs,
+			Polearms,
+			Spears,
+			Blocking,
+			Axes,
+			Bows,
+			FireMagic,
+			FrostMagic,
+			Unarmed,
+			Pickaxes,
+			WoodCutting,
+			Jump = 100,
+			Sneak,
+			Run,
+			Swim,
+			All = 999
+		}
+	}
 }
